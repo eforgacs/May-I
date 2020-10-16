54a602ca428ce5d8e52fed3c81b31638b4058dda
Edward Forgacs <eddie.forgacs@gmail.com>
2020-10-16 10:45:30 -0400

    Convert victory cards from dictionary to stack

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from collections import Counter
 
@@ -6,27 +7,34 @@ import pydealer
 
 class VictoryConditions:
     @staticmethod
-    def two_three_of_a_kind(cards, victory_cards):
+    def two_three_of_a_kind(cards, victory_cards_stack):
+        can_go_down = False
+        victory_cards_type_count = {}
         count = Counter([card.value for card in cards])
         wild_cards = {k: v for k, v in count.items() if k == '2'}
         three_of_a_kinds = {k: v for k, v in count.items() if v >= 3 and k != '2'}
         if not wild_cards:
-            victory_cards |= three_of_a_kinds
-            return len(three_of_a_kinds) >= 2
+            # two natural three of a kinds
+            if len(three_of_a_kinds) >= 2:
+                victory_cards_type_count = three_of_a_kinds
+                can_go_down = True
         else:
             # cannot have two wild cards and 1 natural, so these are the only cases to account for
             two_of_a_kinds = {k: v for k, v in count.items() if v >= 2 and k != '2'}
-            if three_of_a_kinds and two_of_a_kinds and wild_cards:
-                # one natural three of a kind, and one wild with a deuce
-                victory_cards |= two_of_a_kinds
-                victory_cards |= wild_cards
-                victory_cards |= three_of_a_kinds
-                return two_of_a_kinds and wild_cards and three_of_a_kinds
-            elif len(two_of_a_kinds) >= 2 or len(three_of_a_kinds) >= 2:
-                # two wild three of a kinds with deuces
-                victory_cards |= two_of_a_kinds
-                victory_cards |= wild_cards
-                return len(two_of_a_kinds) >= 2 and wild_cards['2'] >= 2
+            if two_of_a_kinds:
+                if three_of_a_kinds:
+                    # one natural three of a kind, and one wild with a deuce
+                    victory_cards_type_count = {**wild_cards, **two_of_a_kinds, **three_of_a_kinds}
+                    can_go_down = True
+                elif len(two_of_a_kinds) >= 2 and wild_cards['2'] >= 2:
+                    # two wild three of a kinds with deuces
+                    victory_cards_type_count = {**wild_cards, **two_of_a_kinds}
+                    can_go_down = True
+        # however many victory cards there are
+        for card in cards:
+            if card.value in victory_cards_type_count.keys():
+                victory_cards_stack.add(card)
+        return can_go_down
 
 
 class Opponent:
@@ -35,8 +43,20 @@ class Opponent:
         self.hand = pydealer.Stack()
 
 
+def prompt_to_choose_card(msg, cards, skip_wilds=False):
+    for index, card in enumerate(cards):
+        if skip_wilds:
+            if card.value != '2':
+                print(f"{index}: {card}")
+        else:
+            print(f"{index}: {card}")
+    entry = input(msg)
+    return entry
+
+
 class Game:
     def __init__(self):
+
         self.verbose = False
 
         self.deck = pydealer.Deck()
@@ -45,7 +65,8 @@ class Game:
         self.hand = self.deck.deal(11)
         self.hand.sort()
 
-        self.victory_cards = {}
+        self.victory_cards = pydealer.Stack()
+        self.victory_card_values = set([card.value for card in self.victory_cards.cards])
 
         self.down = False
         self.down_cards = pydealer.Stack()
@@ -105,56 +126,81 @@ class Game:
             print(f"Discard pile (bottom to top):\n{self.discard_pile}\n")
         else:
             print(f"Discard pile:\n{self.discard_pile[len(self.discard_pile) - 1]}\n")
+        if self.down_cards:
+            print(f"Your down cards:\n{self.down_cards}")
 
     def prompt_to_go_down(self):
-        print("You may go down.\n")
-        go_down = input("Will you go down?\n"
-                        "1. Yes\n"
-                        "2. No\n")
-        if go_down == '1':
-            if self.round == 1:
-                self.down_cards.add(self.victory_cards)
-                for card in list(self.hand.cards):
-                    if card.value in self.victory_cards.keys():
-                        self.hand.cards.remove(card)
-            self.down = True
-        elif go_down == '2':
+        self.victory_card_values = set([card.value for card in self.victory_cards.cards])
+        print("You may go down using a subset of the following cards:\n")
+        for card in self.victory_cards:
+            print(f"{card}")
+        go_down_response = input("Will you go down?\n"
+                                 "1. Yes\n"
+                                 "2. No\n")
+        if go_down_response == '1':
+            self.go_down()
+        elif go_down_response == '2':
             self.down = False
         else:
             print("Invalid entry.\n")
 
+    def go_down(self):
+        if self.round == 1:
+            # 2 x 3 of a kind
+            if 1 <= len(self.victory_card_values) <= 2:
+                print("Auto-selecting down cards.")
+                self.auto_select_down_cards()
+            else:
+                cards_to_use_to_go_down = prompt_to_choose_card("Which cards will you use to go down?\n", self.victory_cards, skip_wilds=True)
+                self.down_cards.add(cards_to_use_to_go_down)
+                for down_card_index in cards_to_use_to_go_down:
+                    del self.hand.cards[(int(down_card_index))]
+        self.down = True
+
+    def auto_select_down_cards(self):
+        for card in list(self.hand.cards):
+            if card in self.victory_cards.cards:
+                self.down_cards.add(card)
+                self.hand.cards.remove(card)
+
     def prompt_for_card_draw(self):
-        card_to_draw = input("Will you draw a card from:\n"
-                             "1. The deck?\n"
-                             "2. The discard pile?\n")
-        if card_to_draw == '1':
-            new_card = self.deck.deal()
-            print(f"\nYou picked up: {new_card}\n")
-            self.hand.add(new_card)
-            self.hand.sort()
-        elif card_to_draw == '2':
-            new_card = self.discard_pile.deal()
-            print(f"You picked up: {new_card}\n")
-            self.hand.add(new_card)
-            self.hand.sort()
-        else:
-            print("Invalid entry.\n")
+        while len(self.hand.cards) == 11:
+            card_to_draw = input("Will you draw a card from:\n"
+                                 "1. The deck?\n"
+                                 "2. The discard pile?\n")
+            if card_to_draw == '1':
+                new_card = self.deck.deal()
+                print(f"\nYou picked up: {new_card}\n")
+                self.hand.add(new_card)
+                self.hand.sort()
+            elif card_to_draw == '2':
+                new_card = self.discard_pile.deal()
+                print(f"You picked up: {new_card}\n")
+                self.hand.add(new_card)
+                self.hand.sort()
+            else:
+                print("Invalid entry.\n")
 
     def prompt_for_discard(self):
         print("Discard one card.\n")
-        for index, card in enumerate(self.hand):
-            print(f"{index}: {card}")
-        discard_entry = input("Which card will you discard?\n")
+        discard_entry = prompt_to_choose_card("Which card will you discard?\n", self.hand)
+        if not discard_entry:
+            print("Empty entry. Please try again.")
         # TODO: Validate entries.
         # if discard_entry not in range(len(self.hand.cards)):
         #     print("Invalid entry.\n")
-        discarded_card = self.discard(discard_entry, self.hand)
-        print(f"You discarded the {discarded_card}.\n")
+        try:
+            discarded_card = self.discard(discard_entry, self.hand)
+            print(f"You discarded the {discarded_card}.\n")
+        except ValueError as e:
+            logging.error(e)
+            print("Invalid entry. Please try again.")
 
     def discard(self, discard_index, hand):
+        # TODO: Validate entries.
         discarded_card = hand.cards[int(discard_index)]
         self.discard_pile.add(discarded_card)
-        hand.cards.__delitem__(int(discard_index))
+        del hand.cards[int(discard_index)]
         return discarded_card
 
 
--- test_main.py
+++ test_main.py
@@ -1,14 +1,17 @@
+from collections import deque
 from unittest import TestCase
+from unittest.mock import patch
 
 import pydealer
+from pydealer import Card, Stack
 
-from src.main import VictoryConditions
+from src.main import VictoryConditions, Game
 
 
 class TestVictoryConditions(TestCase):
     def setUp(self) -> None:
         self.hand = pydealer.Stack()
-        self.victory_cards = {}
+        self.victory_cards = pydealer.Stack()
 
     def test_two_three_of_a_kind_empty(self):
         self.assertFalse(VictoryConditions.two_three_of_a_kind(self.hand.cards, self.victory_cards))
@@ -42,3 +45,91 @@ class TestVictoryConditions(TestCase):
                  pydealer.card.Card('4', 'hearts')]
         self.hand.add(fours)
         self.assertTrue(VictoryConditions.two_three_of_a_kind(self.hand.cards, self.victory_cards))
+
+    def test_two_three_of_a_kind_non_match(self):
+        """Test hands that do not satisfy the victory condition of two three of a kinds."""
+        mix = [pydealer.card.Card('3', 'clubs'), pydealer.card.Card('4', 'diamonds'),
+               pydealer.card.Card('5', 'hearts'), pydealer.card.Card('6', 'spades')]
+        self.hand.add(mix)
+        self.assertFalse(VictoryConditions.two_three_of_a_kind(self.hand.cards, self.victory_cards))
+
+
+class TestGame(TestCase):
+    def setUp(self) -> None:
+        self.game = Game()
+        self.game.hand.empty()
+        self.threes_and_fours_hand = Stack(cards=deque(
+            [Card(value='3', suit='Clubs'), Card(value='3', suit='Diamonds'), Card(value='3', suit='Hearts'),
+             Card(value='4', suit='Clubs'), Card(value='4', suit='Diamonds'), Card(value='4', suit='Hearts')]))
+
+    def test_discard(self):
+        all_spades = [pydealer.card.Card('2', 'spades'),
+                      pydealer.card.Card('3', 'spades'),
+                      pydealer.card.Card('4', 'spades'),
+                      pydealer.card.Card('5', 'spades'),
+                      pydealer.card.Card('6', 'spades'),
+                      pydealer.card.Card('7', 'spades'),
+                      pydealer.card.Card('8', 'spades'),
+                      pydealer.card.Card('9', 'spades'),
+                      pydealer.card.Card('10', 'spades'),
+                      pydealer.card.Card('Jack', 'spades'),
+                      pydealer.card.Card('Queen', 'spades'),
+                      pydealer.card.Card('King', 'spades'),
+                      pydealer.card.Card('Ace', 'spades')]
+        self.game.hand.add(all_spades)
+        self.assertEqual(pydealer.card.Card('Ace', 'spades'), self.game.discard(12, self.game.hand))
+        self.assertEqual([pydealer.card.Card('2', 'spades'),
+                          pydealer.card.Card('3', 'spades'),
+                          pydealer.card.Card('4', 'spades'),
+                          pydealer.card.Card('5', 'spades'),
+                          pydealer.card.Card('6', 'spades'),
+                          pydealer.card.Card('7', 'spades'),
+                          pydealer.card.Card('8', 'spades'),
+                          pydealer.card.Card('9', 'spades'),
+                          pydealer.card.Card('10', 'spades'),
+                          pydealer.card.Card('Jack', 'spades'),
+                          pydealer.card.Card('Queen', 'spades'),
+                          pydealer.card.Card('King', 'spades')], self.game.hand)
+
+    def test_auto_select_down_cards_two_threes(self):
+        # Round 1: 2 x 3 of a kind
+        threes = [pydealer.card.Card('3', 'clubs'), pydealer.card.Card('3', 'diamonds'),
+                  pydealer.card.Card('3', 'hearts')]
+        self.game.hand.add(threes)
+        fours = [pydealer.card.Card('4', 'clubs'), pydealer.card.Card('4', 'diamonds'),
+                 pydealer.card.Card('4', 'hearts')]
+        self.game.hand.add(fours)
+
+        self.assertEqual(Stack(), self.game.down_cards)
+        self.assertEqual(self.threes_and_fours_hand, self.game.hand)
+        VictoryConditions.two_three_of_a_kind(self.game.hand.cards, self.game.victory_cards)
+        self.game.auto_select_down_cards()
+
+        self.assertEqual(self.threes_and_fours_hand, self.game.down_cards)
+        self.assertEqual(Stack(), self.game.hand)
+
+    @patch('builtins.input', return_value='2')
+    def test_prompt_to_go_down(self, mock_input):
+        self.game.down = True
+        self.game.prompt_to_go_down()
+        self.assertEqual(False, self.game.down)
+
+    # def test_go_down_auto_select(self):
+    #     self.game.hand.add(self.threes_and_fours_hand)
+    #     VictoryConditions.two_three_of_a_kind(self.game.hand.cards, self.game.victory_cards)
+    #     self.game.go_down()
+    #     self.assertEqual(Stack(), self.game.hand)
+    #     self.assertEqual(self.threes_and_fours_hand, self.game.down_cards)
+
+    # def test_go_down_manual_select(self):
+    #     threes = [pydealer.card.Card('3', 'clubs'), pydealer.card.Card('3', 'diamonds'),
+    #               pydealer.card.Card('3', 'hearts')]
+    #     self.game.hand.add(threes)
+    #     fours = [pydealer.card.Card('2', 'clubs'), pydealer.card.Card('4', 'diamonds'),
+    #              pydealer.card.Card('4', 'hearts')]
+    #     self.game.hand.add(fours)
+    #     fives = [pydealer.card.Card('5', 'clubs'), pydealer.card.Card('5', 'diamonds'),
+    #               pydealer.card.Card('5', 'hearts')]
+    #     self.game.hand.add(fives)
+    #     VictoryConditions.two_three_of_a_kind(self.game.hand.cards, self.game.victory_cards)
+    #     self.game.go_down()
